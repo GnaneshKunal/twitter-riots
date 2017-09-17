23d8f88a366173e4745f6eb714deb3775ff5bbdc
gnanesh <gnaneshkunal@outlook.com>
2017-09-17 12:42:18 +0530

    Initial Commit

---
+++ src/components/Main.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Home from './Home';
+
+const Main = (): JSX.Element => (
+    <div>
+        <Home message='Twitter Hash Riot' author='Gnanesh' />
+    </div>
+);
+
+export default Main;
