fe1fb89ddd81b532e67905206774cc8931f453f0
gnanesh <gnaneshkunal@outlook.com>
2017-10-07 16:19:56 +0530

    add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+    return (
+        <div>
+            <Link to="/trends">Trends</Link>
+        </div>
+    );
+}
