7268b719fb093966ce036f292c02ca05c6571a07
gnanesh <gnaneshkunal@outlook.com>
2017-10-09 00:53:28 +0530

    add searchFOrm

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import Home from './Home';
+import SearchTrendsForm from './SearchTrendsForm';
 
 const Main = (): JSX.Element => (
     <div>
-        <Home message='Twitter Hash Riot' author='Gnanesh' />
+        {/* <Home message='Twitter Hash Riot' author='Gnanesh' /> */}
+        <SearchTrendsForm />
     </div>
 );
 
