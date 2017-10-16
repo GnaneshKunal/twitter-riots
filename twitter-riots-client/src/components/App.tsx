523998469a05257b070f3ad0eae0a1c6c3278053
gnanesh <gnaneshkunal@outlook.com>
2017-10-16 20:10:08 +0530

    add tweets & trends route

--- twitter-riots-client/src/components/App.tsx
+++ twitter-riots-client/src/components/App.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Main from './Main';
-import Tweets from './HashTagTweets';
+import HashTweets from './HashTagTweets';
+import Trends from './Trends';
 import Header from './Header';
+import { connect, Dispatch } from 'react-redux';
 
 const App = (): JSX.Element  => (
     <div>
@@ -11,7 +13,8 @@ const App = (): JSX.Element  => (
         <main>
             <Switch>
                 <Route exact path='/' component={Main} />
-                <Route exact path='/tweets/:hash' component={Tweets} />
+                <Route exact path='/tweets/:hash' component={HashTweets} />
+                <Route exact path='/trends/:location' component={Trends} />
             </Switch>
         </main>
     </div>
