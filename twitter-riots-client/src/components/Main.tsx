e6c5e3551b6c2489f111486efec165fff4f3ca4a
gnanesh <gnaneshkunal@outlook.com>
2017-10-17 15:48:47 +0530

    Testing maps

--- twitter-riots-client/src/components/Main.tsx
+++ twitter-riots-client/src/components/Main.tsx
@@ -2,9 +2,16 @@ import * as React from 'react';
 import Home from './Home';
 import SearchTrendsForm from './SearchTrendsForm';
 
+import Map from './Map';
+import Map2 from './Map2';
+
+import TrendsMap from './TrendsMap';
+
 const Main = (): JSX.Element => (
     <div>
-        {/* <Home message='Twitter Hash Riot' author='Gnanesh' /> */}
+        <Map />
+       {/*<Map2 />*/}
+       {/*<TrendsMap />*/}
         <SearchTrendsForm />
     </div>
 );
