f759a571ae24814a331767d2610cc13f2468ab80
gnanesh <gnaneshkunal@outlook.com>
2017-10-17 23:59:29 +0530

    Testing Map

--- twitter-riots-client/src/components/TrendsResult.tsx
+++ twitter-riots-client/src/components/TrendsResult.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
+import TrendsResultMap from './TrendsResultMap';
+
 interface searchDataSchema {
     name: string,
     query: string,
@@ -12,7 +14,8 @@ interface ISearchDataProps {
     geo: {
         lat: Number,
         long: Number,
-        woeid: Number
+        woeid: Number,
+        location: String
     }
 }
 
@@ -21,7 +24,8 @@ export default class Home extends React.Component<ISearchDataProps, {}> {
     public render(): JSX.Element {
         return (
             <div> 
-
+                <h3><center> Nearest Trends: <b>{this.props.geo.location}</b></center></h3>
+                <TrendsResultMap />
                 <div className="list-group">
                     {this.props.searchData.map(x => {
                         return <Link to={`/tweets/${x.query}?lat=${this.props.geo.lat}&long=${this.props.geo.long}&woeid=${this.props.geo.woeid}`} className="list-group-item list-group-item-action" id={x.name}>{x.name}</Link>
