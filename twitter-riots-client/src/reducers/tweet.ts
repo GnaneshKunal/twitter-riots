c365ba3183824325e403aad069e6fb3cc9c63aba
gnanesh <gnaneshkunal@outlook.com>
2017-11-02 20:10:34 +0530

    reduce tweet sentiments

--- twitter-riots-client/src/reducers/tweet.ts
+++ twitter-riots-client/src/reducers/tweet.ts
@@ -8,7 +8,14 @@ interface IActionProps {
 export default function(state = {}, action: IActionProps) {
     switch(action.type) {
         case TRENDING_TWEETS:
-            return { ...state, search: true, tweetData: action.payload };
+            return {
+                ...state, search: true,
+                tweetData: action.payload.tweets,
+                sentiment: action.payload.sentiment,
+                pos: action.payload.pos,
+                neg: action.payload.neg,
+                neu: action.payload.neu
+            };
     }
     
     return state;
